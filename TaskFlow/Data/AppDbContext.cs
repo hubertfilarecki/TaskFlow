22f400d56523bbc850ed2024810494959acee1c4
Hubert Filarecki <tygras.yt@gmail.com>
2026-08-05 00:10:23 +0200

    Konfiguracja Entity Framework Core i utworzenie bazy danych

---
+++ TaskFlow/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TaskFlow.Models;
+
+namespace TaskFlow.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Project> Projects { get; set; }
+    }
+}
